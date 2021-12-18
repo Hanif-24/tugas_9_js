ad390ba8d9c6ae4110a58df17fea692ad5c39847
Hanif-Niomic <m.alihanif249@gmail.com>
2021-12-18 19:57:28 +0700

    Tugas 9 Js
    Tugas 9 Js

---
+++ tugas9.js
@@ -0,0 +1,15 @@
+function tugas9(){
+  var mahasiswa = {
+    nama: 'Hanif',
+    nim: 1118093070,
+    semester: 7,
+    prodi: 'Sistem Informasi',
+    univ: 'UIN Jakarta',
+
+  }
+  for(var x in mahasiswa){
+    console.log(mahasiswa[x]);
+  }
+}
+
+tugas9()
